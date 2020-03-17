c9a8fae9771adc94c8b31f61d17358fa3432745e
tyler-baldwin <tyler.baldwin.17@cnu.edu>
2020-03-17 17:06:57 -0400

    fuck this bug

--- print_ts.h
+++ print_ts.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef PRINT_TS_H_
+#define PRINT_TS_H_
 #include <string>
 
 //create threadsafe implementations of the following functions
@@ -15,3 +17,5 @@ void PRINT2(std::string &txt, std::string &txt1);
 void PRINT3(std::string &txt, std::string &txt1, std::string &txt2);
 void PRINT4(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3);
 void PRINT5(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3, std::string &txt4);
+
+#endif /* PRINT_TS_H_ */
--- tester.cpp
+++ tester.cpp
@@ -5,7 +5,13 @@
  *      Author: Tyler Baldwin
  */
 #include "tester.h"
-
+#include <vector>
+#include <thread>
+#include <iostream>
+#include "print_ts.h"
+//stores running and stopped threads
+std::vector<std::thread> vecThreads;
+bool pleaseStop;
 /*
  * starts cancelable threads
  * string s			-the string to print
@@ -16,6 +22,30 @@
  */
 void startThreads(std::string s, int numThreads, WHICH_PRINT wp,
 		int numTimesToPrint, int millisecond_delay) {
+
+
+	vecThreads.push_back( std::thread (PRINT1,s));
+
+
+
+//	for (int i = 0; i < numThreads / 2; i++) {
+//		switch (wp) {
+//		case P1: {
+//			std::thread p1 (PRINT1, s);
+//			vecThreads.push_back(p1);
+//		}
+//			break;
+//		case P2:
+//			break;
+//		case P3:
+//			break;
+//		case P4:
+//			break;
+//		case P5:
+//			break;
+//
+//		}
+
 
 }
 
@@ -31,9 +61,19 @@ void setCancelThreads(bool bCancel) {
  * waits for all threads to complete
  */
 void joinThreads() {
-
+	for (auto &t : vecThreads) {
+		t.join();
+	}
 }
 
-int main(){
+int main() {
+	pleaseStop = false;
+	int numbThreads = std::thread::hardware_concurrency();
+	vecThreads = { };
+
+	startThreads("A", numbThreads, P2, 4, 1000);
+	startThreads("B", numbThreads, P3, 5, 1000);
+
+	joinThreads();
 
 }
